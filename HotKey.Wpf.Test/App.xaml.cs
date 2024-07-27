76cccda2ed60b3e029c5a4239017159cbcd51cc9
AlexSJC <cn_sjc@outlook.com>
2024-07-27 22:36:38 +0800

    update

---
+++ HotKey.Wpf.Test/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Asjc.HotKey.Wpf.Test
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            new HotKey(Key.Space, Modifiers.Ctrl, _ => MessageBox.Show("Ctrl + Space"));
+        }
+    }
+
+}
