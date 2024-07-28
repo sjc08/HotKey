040a03d7b29cfb247ce7c43258bf1600a4fde953
AlexSJC <cn_sjc@outlook.com>
2024-07-28 13:21:46 +0800

    add comments

--- HotKey/HotKeyBase.cs
+++ HotKey/HotKeyBase.cs
@@ -5,35 +5,68 @@ namespace Asjc.HotKey
 {
     public abstract partial class HotKeyBase
     {
+        /// <summary>
+        /// Posted when the user presses a hot key registered by <see cref="RegisterHotKey(nint, int, Modifiers, uint)"/>.
+        /// </summary>
         protected const int WM_HOTKEY = 0x0312;
+
+
         protected static readonly Dictionary<int, HotKeyBase> map = [];
 
+        /// <summary>
+        /// Defines a system-wide hot key.
+        /// </summary>
         [LibraryImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         protected static partial bool RegisterHotKey(IntPtr hWnd, int id, Modifiers fsModifiers, uint vk);
 
+        /// <summary>
+        /// Frees a hot key previously registered by the calling thread.
+        /// </summary>
         [LibraryImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         protected static partial bool UnregisterHotKey(IntPtr hWnd, int id);
 
+        /// <summary>
+        /// An <see langword="int"/> indicating the id of the hot key.
+        /// </summary>
         [JsonIgnore]
         public int Id { get; protected set; }
 
+        /// <summary>
+        /// A <see langword="bool"/> indicating whether the hot key has been registered.
+        /// </summary>
         [JsonIgnore]
         public bool Registered { get; protected set; }
 
+        /// <summary>
+        /// The virtual-key code of the hot key.
+        /// </summary>
         public uint Key { get; init; }
 
+        /// <summary>
+        /// The keys that must be pressed in combination with the key specified by <see cref="Key"/> in order to generate the <see cref="WM_HOTKEY"/> message.
+        /// </summary>
         public Modifiers Modifier { get; init; }
 
+        /// <summary>
+        /// Occurs when the hot key is pressed.
+        /// </summary>
         public event Action<HotKeyBase>? Pressed;
 
+        /// <summary>
+        /// Register the hot key.
+        /// </summary>
+        /// <returns>
+        /// A <see langword="bool"/> indicating whether the operation was successful.
+        /// </returns>
         public virtual bool Register()
         {
             if (Registered) return true;
             int id = (int)Modifier << 16 | (int)Key;
             if (RegisterHotKey(IntPtr.Zero, id, Modifier, Key))
             {
+                // If successful.
                 Id = id;
                 Registered = true;
                 map.Add(Id, this);
@@ -42,11 +75,18 @@ namespace Asjc.HotKey
             return false;
         }
 
+        /// <summary>
+        /// Unregister the hot key.
+        /// </summary>
+        /// <returns>
+        /// A <see langword="bool"/> indicating whether the operation was successful.
+        /// </returns>
         public virtual bool Unregister()
         {
             if (!Registered) return false;
             if (UnregisterHotKey(IntPtr.Zero, Id))
             {
+                // If successful.
                 Id = default;
                 Registered = false;
                 map.Remove(Id);
@@ -55,6 +95,9 @@ namespace Asjc.HotKey
             return false;
         }
 
+        /// <summary>
+        /// Handle a hot key with the specified id by invoking <see cref="Pressed"/>.
+        /// </summary>
         protected static bool Handle(int id)
         {
             if (map.TryGetValue(id, out var hotKey))
--- HotKey/Modifiers.cs
+++ HotKey/Modifiers.cs
@@ -4,10 +4,30 @@
     public enum Modifiers : uint
     {
         None = 0x0000,
+
+        /// <summary>
+        /// Either ALT key must be held down.
+        /// </summary>
         Alt = 0x0001,
+
+        /// <summary>
+        /// Either CTRL key must be held down.
+        /// </summary>
         Ctrl = 0x0002,
+
+        /// <summary>
+        /// Either SHIFT key must be held down.
+        /// </summary>
         Shift = 0x0004,
+
+        /// <summary>
+        /// Either WINDOWS key must be held down.
+        /// </summary>
         Win = 0x0008,
+
+        /// <summary>
+        /// Changes the hotkey behavior so that the keyboard auto-repeat does not yield multiple hotkey notifications.
+        /// </summary>
         NoRepeat = 0x4000
     }
 }
